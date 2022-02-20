717870fbb2fcb86baecc40c45701a4f18b37f145
Jlee108 <jbacked@yahoo.com>
2022-02-20 19:43:33 +0000

    2nd iteration

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,15 +1,6 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        //first declare the length of the string
-        int sizeOfString = inputString.length();
-        //breaking the string into each character and assign that to the index value
-        //reversing the order of the index
-        StringBuilder sb = new StringBuilder();
-        for (int i = sizeOfString - 1; i >= 0; i--) {
-
-            sb.append(inputString.charAt(i));
-        }
-        return sb.toString();
+        return new StringBuilder(inputString).reverse().toString();
     }
 }
