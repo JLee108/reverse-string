3b712cc64c0b13ee0c0e296ba2d34b16b7005731
Jlee108 <jbacked@yahoo.com>
2022-02-20 19:39:28 +0000

    All tests pass

--- src/main/java/ReverseString.java
+++ src/main/java/ReverseString.java
@@ -1,7 +1,15 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        //first declare the length of the string
+        int sizeOfString = inputString.length();
+        //breaking the string into each character and assign that to the index value
+        //reversing the order of the index
+        StringBuilder sb = new StringBuilder();
+        for (int i = sizeOfString - 1; i >= 0; i--) {
+
+            sb.append(inputString.charAt(i));
+        }
+        return sb.toString();
     }
-  
 }
